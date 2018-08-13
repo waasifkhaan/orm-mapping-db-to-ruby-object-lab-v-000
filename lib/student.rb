4ca3020cb355a2e11ff710729af8203416502d47
Wasif Khan <waasif.khaan@gmail.com>
2018-08-13 23:49:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,6 +78,7 @@ class Student
     DB[:conn].execute(sql,12).map do |row|
       self.new_from_db(row)
     end
+  end 
     def self.first_X_students_in_grade_10
       sql = <<-SQL
       SELECT * FROM students
