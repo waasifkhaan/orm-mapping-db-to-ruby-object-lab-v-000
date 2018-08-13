9e21d1b94138dcbdf02c245dc2d49204b953dda8
Wasif Khan <waasif.khaan@gmail.com>
2018-08-13 23:52:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,7 +79,8 @@ class Student
       self.new_from_db(row)
     end
   end 
-    def self.first_X_students_in_grade_10(X)
+  
+  def self.first_X_students_in_grade_10(X)
       sql = <<-SQL
       SELECT ? FROM students
       WHERE grade = ? 
