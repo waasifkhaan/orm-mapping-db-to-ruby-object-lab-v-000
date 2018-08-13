535a6fdfac66b0dfc22d38d14f2e3840b09f3b72
Wasif Khan <waasif.khaan@gmail.com>
2018-08-13 23:59:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,6 +89,16 @@ class Student
     end
   end 
   
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql,x).map do |row|
+      self.new_from_db(row)
+    end
+  end 
+  
   def self.first_X_students_in_grade_10(x)
       sql = <<-SQL
       SELECT * FROM students
